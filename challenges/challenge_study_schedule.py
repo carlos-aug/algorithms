389dca83fc9bdf266f16d3bf45446fd727228beb
Carlos Augusto <caarlos.san@gmail.com>
2023-05-03 19:19:35 -0300

    feat: add req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count_hour = 0
+    if not target_time:
+        return None
+
+    for i, j in permanence_period:
+        if not i or not j or type(i) == str or type(j) == str:
+            return None
+        elif target_time >= i and target_time <= j:
+            count_hour += 1
+
+    return count_hour
